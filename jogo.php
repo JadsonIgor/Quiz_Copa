488bc787863d8bd9d0dfcead857d3ab0464771cf
JadsonIgor <112197230+JadsonIgor@users.noreply.github.com>
2022-08-26 14:35:10 -0300

    Add files via upload

---
+++ jogo.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quiz - Copa do Mundo 2022</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Signika+Negative&family=Uchen&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Quiz - Copa do Mundo 2022 - Seleção Brasileira</h1>
+        <hr>
+        <div class="imagem">
+            <img src="logo.png">
+        </div>
+        <form class="formulario">
+            <label>Nome do jogador:</label>
+            <input type="text" id="name">
+        </form>
+        <div class="btn"><button>INICIAR</button></div>
+    </div>
+</body>
+
+</html>
